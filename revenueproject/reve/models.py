b80d6d8687b4b0b2160aee485c278794e69f20e2
laurasjparis <lsjimenezp@eafit.edu.co>
2025-03-03 23:32:50 -0500

    mas cosas

--- revenueproject/reve/models.py
+++ revenueproject/reve/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Reve(models.Mode):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField (upload_to= 'reve/images/')
+    url = models.URLField(blank=True)
--- revenueproject/revenue/urls.py
+++ revenueproject/revenue/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reve import views as reveViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',reveViews.home),
 ]
