fe448fa95cd546b5610767ec8d8e779fe9f75a64
laurasjparis <lsjimenezp@eafit.edu.co>
2025-03-04 10:41:06 -0500

    Admin django

--- revenueproject/reve/views.py
+++ revenueproject/reve/views.py
@@ -6,11 +6,16 @@ from .models import Reve
 def home(request):
     #return render (request, 'home.html')
     searchTerm = request. GET.get('searchReve')
-    reves = Reve.objects.all()
+    
+    if searchTerm:
+        reves = Reve.objects.filter(title__icontains = searchTerm)
+    else:
+        reves = Reve.objects.all()
     return render(request, 'home.html', {'searchTerm' : searchTerm, 'reves': reves} )
  
 
-
+def about (request):
+    return HttpResponse('<h1>Welcome to about home Page </h1>')
 
 
 # Create your views here.
--- revenueproject/revenue/urls.py
+++ revenueproject/revenue/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('',reveViews.home),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
